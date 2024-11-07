c0ca68827c98fcc49660d42a444e4518b9708f7a
WilliamPhan03 <wphanwilliam03@icloud.com>
2024-11-06 19:12:00 -0800

    added the home page budget chart, and an expense line graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation ("com.google.android.libraries.places:places:3.1.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
 }
